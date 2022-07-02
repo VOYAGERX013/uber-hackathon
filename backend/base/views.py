75792105773931663f5a730e6e624bbaa0f3e92b
VOYAGERX013 <aryhegde@gmail.com>
2022-07-02 12:47:43 +0530

    Setting up a custom database model

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def home(req):
-    return HttpResponse("Hello World !")
+    return HttpResponse("Hello world!")
+
+def register(req):
+    pass
+
+def login(req):
+    pass
