3862fca28b549faf0e2df5076b1f4701977db6ff
Ruby Chang <ruby931109@gmail.com>
2022-07-24 22:48:18 +0800

    loglo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
-import axios from "axios"
+import axios from 'axios'
 import { BsFillPersonFill } from 'react-icons/bs'
 
 export default function Navbar() {
   const [status, setstatus] = useState(false)
-    useEffect(()=>{
-    axios.post('http://localhost:8000/api/get-user/', null, { withCredentials: true })
-        .then(res => {
-          console.log(res.data)
-          setstatus(res.data.success)
-        })
-        .catch(err => console.log(err))
+  useEffect(() => {
+    axios
+      .post('http://localhost:8000/api/get-user/', null, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data)
+        setstatus(res.data.success)
+      })
+      .catch((err) => console.log(err))
   })
 
   return (
     <div className="navbar">
       <Link to="/" className="navbar-logo">
-      {/* <img className="logo" src="/images/logo.png" alt="" /> */}
+        <img className="logo" src="/images/logo.png" alt="" />
       </Link>
       <ul className="nav-m">
         <li className="nav-item">
@@ -41,29 +44,26 @@ export default function Navbar() {
             Extension
           </Link>
         </li>
-        {status?(
-         <li className="nav-item">
-         <Link to="/suggest" className="nav-links">
-           Suggestions
-         </Link>
-       </li>
-        ):(<li>
-
-        </li>
-        
+        {status ? (
+          <li className="nav-item">
+            <Link to="/suggest" className="nav-links">
+              Suggestions
+            </Link>
+          </li>
+        ) : (
+          <li></li>
         )}
-        {status?(
-          <div className = "avatarwrap">
-          <BsFillPersonFill className = "avatar"></BsFillPersonFill>
+        {status ? (
+          <div className="avatarwrap">
+            <BsFillPersonFill className="avatar"></BsFillPersonFill>
           </div>
-        ):(
-        <div className="login">
-          <Link to="/login" className="logindiv">
-            Login
-          </Link>
-        </div>
+        ) : (
+          <div className="login">
+            <Link to="/login" className="logindiv">
+              Login
+            </Link>
+          </div>
         )}
-        
       </ul>
     </div>
   )
